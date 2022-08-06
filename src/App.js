191b66725239700df9eb23b479499e57d2d52317
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2022-08-06 21:12:19 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { StrictMode, useEffect, useRef, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [users,setUsers]=useState([])
+  const nameRef=useRef()
+  const emailRef=useRef()
+ 
+  
+
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res=>res.json())
+    .then(data=>setUsers(data))
+  },[])
+
+  const handleAddUser=e=>{
+   const name=nameRef.current.value;
+    const email=emailRef.current.value;
+    const newUser={name:name,email:email}
+
+  //  send data the server
+  fetch('http://localhost:5000/users',{
+    method:'post',
+    headers:{
+      'content-type':'application/json',
+    },
+    body:JSON.stringify(newUser)
+  })
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data)
+    const addedUser=data;
+     const newUser=[...users,addedUser];
+    setUsers(newUser);
+  });
+  nameRef.current.value='';
+  emailRef.current.value='';
+    e.preventDefault();
+  }
+ 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {users.length}
+
+    <form onSubmit={handleAddUser}>
+      <input type="text" ref={nameRef} placeholder='name' />
+      <input type="email" ref={emailRef} name="" id="" placeholder='email' />
+      <input type="submit" value="submit" />
+    </form>
+
+    {
+      users.map(user=><li key={user.id}>id:{user.id} {user.name}  email:{user.email}</li>)
+    }
+
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOMClient from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
 
 ReactDOM.render(
   <React.StrictMode>
